8c2d9e59a30840281a3a4886ee29d15a55000462
IcelandSpar <victorasielrodriguez@gmail.com>
2024-08-16 16:37:57 -0500

    feat: add minimal css blueprint for UI of game

---
+++ src/dom.js
@@ -0,0 +1,7 @@
+import { Player } from './player.js'
+
+const GridDomRef = {
+    humanGrid: document.querySelector('.grid1'),
+    computerGrid: document.querySelector('.grid2'),
+}
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { Gameboard } from './gameboard.js'
 import './styles.css'
+import { makeGrids } from './dom.js'
 
-const NewBoard = new Gameboard();
-NewBoard.placeShip(0, 0, NewBoard.Cruiser);
-NewBoard.placeShip(9, 9, NewBoard.Destroyer);
-NewBoard.receiveAttack(0, 0);
-NewBoard.receiveAttack(0, 0);
-NewBoard.receiveAttack(5, 5);
-NewBoard.receiveAttack(6, 5);
-console.log(NewBoard.missedAttacks)
-console.table(NewBoard.board)
+// const NewBoard = new Gameboard();
+// NewBoard.placeShip(0, 0, NewBoard.Cruiser);
+// NewBoard.placeShip(9, 9, NewBoard.Destroyer);
+// NewBoard.receiveAttack(0, 0);
+// NewBoard.receiveAttack(0, 0);
+// NewBoard.receiveAttack(5, 5);
+// NewBoard.receiveAttack(6, 5);
+// console.log(NewBoard.missedAttacks)
+// console.table(NewBoard.board)
+
+makeGrids()
